d682673ebe8752bd8e302e6734823e2676f85bec
Kitchie <francismoll@gmail.com>
2019-03-13 00:24:06 +1300

    feat(register): added register module

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -11,6 +11,7 @@ public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args)
+                k
     }
 
 }
